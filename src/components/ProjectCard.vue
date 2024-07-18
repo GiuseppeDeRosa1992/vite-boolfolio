6c167ee59becae95973ef5a8d30ce1832175094f
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-07-18 17:26:28 +0200

    migliorative layout

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -8,7 +8,7 @@ export default {
 
     data() {
         return {
-            msg: "Ciao funziona tutto",
+            msg: "I miei Progetti",
         }
     },
 };
@@ -16,16 +16,19 @@ export default {
 </script>
 
 <template>
-    <h1>{{ msg }}</h1>
-    <div class="row">
+    <h1 class="text-white">{{ msg }}</h1>
+    <div class="row text-white">
         <!-- FACCIO CICLO FOR E CICLO NELLA PROPS CHE POI LA POPOLO IN APP.VUE QUANDO LA RICHIAMO -->
         <template v-for="project in cardProjects">
-            <div class="col-12 col-sm-6 col-md-4 col-lg-2 px-1">
-                <figure class="m-0 h-100">
-                    <img :src="api_url + '/storage/' + project.img_preview" class="card-img-top img-fluid h-75"
-                        :alt="project.img_preview">
-                    <h5 class="card-text my-fs py-2">{{ project.title }}</h5>
-                </figure>
+            <div class="col-12 col-sm-6 col-md-4 col-lg-4 my-3">
+                <div class="card p-0 h-100">
+                    <figure class="m-0 h-100">
+                        <img :src="api_url + '/storage/' + project.img_preview" class="card-img-top"
+                            :alt="project.img_preview">
+                        <h5 class="card-text my-fs py-2">{{ project.title }}</h5>
+                        <p class="card-text my-fs py-2">{{ project.description }}</p>
+                    </figure>
+                </div>
             </div>
         </template>
     </div>

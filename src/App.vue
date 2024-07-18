57356123221b4115552fdec944f6d9bad9b65870
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-07-18 16:38:52 +0200

    add components projectCard taht create card with props and add app.vue that print in page with call axios

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue';
+
 
 export default {
+
+  name: "App",
+
+  components: {
+    ProjectCard,
+  },
+
   data() {
     return {
       messaggio: "ciao sono Giuseppe",
 
       api_url: "http://127.0.0.1:8000",
+
+      projects: [],
     }
   },
 
@@ -16,38 +27,15 @@ export default {
 
   mounted() {
     axios.get(`${this.api_url}/api/projects`).then(result => {
-      console.log(result); //la api funziona
+      //la api funziona
+      this.projects = result.data.projects
     })
   },
 };
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-    <h1>{{ messaggio }}</h1>
-  </div>
-
+  <ProjectCard :cardProjects="projects.data" :api_url="api_url" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+
+    name: "ProjectCard",
+
+    //CREO LA PROPS
+    props: ["cardProjects", "api_url"],
+
+    data() {
+        return {
+            msg: "Ciao funziona tutto",
+        }
+    },
+};
+
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+    <div class="row">
+        <!-- FACCIO CICLO FOR E CICLO NELLA PROPS CHE POI LA POPOLO IN APP.VUE QUANDO LA RICHIAMO -->
+        <template v-for="project in cardProjects">
+            <div class="col-12 col-sm-6 col-md-4 col-lg-2 px-1">
+                <figure class="m-0 h-100">
+                    <img :src="api_url + '/storage/' + project.img_preview" class="card-img-top img-fluid h-75"
+                        :alt="project.img_preview">
+                    <h5 class="card-text my-fs py-2">{{ project.title }}</h5>
+                </figure>
+            </div>
+        </template>
+    </div>
+</template>
+
+<style scoped></style>

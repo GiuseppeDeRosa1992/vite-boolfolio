f4bbce2b6abdcf050600d1fd6bd796dad5120e30
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-07-19 18:30:27 +0200

    import router.js, main.js and store that create array for link

--- src/data/store.js
+++ src/data/store.js
@@ -3,6 +3,7 @@ import { reactive } from 'vue';
 const iMieiDati = reactive({
     projects: [],
     api_url: "http://127.0.0.1:8000",
+    arrayLink: ["home", "contacts", "projects", "about"],
 });
 
 export default iMieiDati;
--- src/router.js
+++ src/router.js
@@ -1,4 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router.js';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import AppHome from './views/AppHome.vue';
+import AppAbout from './views/AppAbout.vue';
+import AppContacts from './views/AppContacts.vue';
+import AppProjects from './views/AppProjects.vue';
+
 
 const router = createRouter({
     history: createWebHistory(),
@@ -6,25 +12,27 @@ const router = createRouter({
         {
             path: "/",
             name: "home",
-            component: "AppHome"
+            component: AppHome
         },
 
         {
             path: "/about",
             name: "about",
-            component: "AppAbout"
+            component: AppAbout
         },
 
         {
             path: "/projects",
             name: "projects",
-            component: "AppProjects"
+            component: AppProjects
         },
 
         {
             path: "/contacts",
             name: "contacts",
-            component: "AppContacts"
+            component: AppContacts
         }
     ]
 });
+
+export { router }

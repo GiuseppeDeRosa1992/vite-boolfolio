782d2ee2c60a193b95d55492fb11b49169c6a2e0
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-07-22 15:48:41 +0200

    addd new variable in store for array latest.projects and for api call variable latest

--- src/data/store.js
+++ src/data/store.js
@@ -2,8 +2,15 @@ import { reactive } from 'vue';
 
 const iMieiDati = reactive({
     projects: [],
+
+    //creo la variabile per la lista degli ultimi progetti
+    latestProjects: [],
+
     api_url: "http://127.0.0.1:8000",
     arrayLink: ["home", "contacts", "projects", "about"],
+
+    //creo variabile per richiamare api projectslatest che mi restituisce gli ultimi 6 progetti
+    latest: "/api/projects/latest",
 });
 
 export default iMieiDati;

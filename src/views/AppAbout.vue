ca9b27aa96482d494ec2b91371883cbb4f50c990
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-07-23 19:05:55 +0200

    implemented AppAbout with my data and send email with form

--- src/views/AppAbout.vue
+++ src/views/AppAbout.vue
@@ -5,7 +5,7 @@ export default {
 
     data() {
         return {
-            title: "Ciao questa è la about"
+
         }
     },
 };
@@ -14,7 +14,61 @@ export default {
 
 <template>
     <section>
-        <h1 class="text-white">{{ title }}</h1>
+        <!-- <h1 class="text-white">{{ title }}</h1> -->
+        <div class="container text-white py-2">
+            <h2 class="m-0 text-center">I miei Dati</h2>
+            <div class="row m-0 g-3 text-center justify-content-center">
+                <div class="col-6">
+                    <h5 class="m-0 py-1">Nome</h5>
+                    <p class="m-0 py-1">Giuseppe</p>
+                </div>
+                <div class="col-6">
+                    <h5 class="m-0 py-1">Cognome</h5>
+                    <p class="m-0 py-1">De Rosa</p>
+                </div>
+                <div class="col-6">
+                    <h5 class="m-0 py-1">Data di Nascita:</h5>
+                    <p class="m-0 py-1">10/09/1992</p>
+                </div>
+                <div class="col-6">
+                    <h5 class="m-0 py-1">Età</h5>
+                    <p class="m-0 py-1">32 Anni</p>
+                </div>
+                <div class="col-6">
+                    <h5 class="m-0 py-1">Indirizzo di Residenza</h5>
+                    <p class="m-0 py-1">Via Borgo Trento Trieste 27, Camposampiero 35012 (PD), ITALIA.</p>
+                </div>
+                <div class="col-6 text-center">
+                    <div class="row m-0 justify-content-center">
+                        <div class="col-10 p-0">
+                            <h5 class=" py-1">Lingue Parlate</h5>
+                            <table class="table m-0">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Lingua</th>
+                                        <th scope="col">Livello</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Italiano</td>
+                                        <td>Madrelingua</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Tedesco</td>
+                                        <td>Ottimo</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Inglese</td>
+                                        <td>Base</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
 
--- src/views/AppContacts.vue
+++ src/views/AppContacts.vue
@@ -1,93 +1,104 @@
 <script>
+
+
+import axios from 'axios';
+
+
 export default {
 
     name: "AppContacts",
 
     data() {
         return {
-            // title: "Ciao questa è la Contacts"
-
+            name: '',
+            email: '',
+            message: '',
+            errors: {},
+            loading: false,
+            success: false
         }
     },
+    methods: {
+        sendMessage() {
+            this.loading = true;
+
+            const data = {
+                'name': this.name,
+                'email': this.email,
+                'message': this.message,
+            }
+            console.log(data);
+
+            this.errors = {};
+            axios
+                .post("http://127.0.0.1:8000/api/contacts", data)
+                .then(response => {
+                    console.log(response);
+                    this.success = response.data.success
+                    if (this.success) {
+                        // vedi il messaggio
+                        this.name = '';
+                        this.email = '';
+                        this.message = '';
+                    } else {
+                        // messaggio di errore
+                        this.errors = response.data.errors
+                    }
+
+                    this.loading = false;
+                })
+
+        }
+    }
 };
 
 </script>
 
 <template>
     <section>
-        <!-- <h1 class="text-white">{{ title }}</h1> -->
-        <div class="container text-white py-2">
-            <h2 class="m-0 text-center">I miei Dati</h2>
-            <div class="row m-0 g-3 text-center justify-content-center">
-                <div class="col-6">
-                    <h5 class="m-0 py-1">Nome</h5>
-                    <p class="m-0 py-1">Giuseppe</p>
-                </div>
-                <div class="col-6">
-                    <h5 class="m-0 py-1">Cognome</h5>
-                    <p class="m-0 py-1">De Rosa</p>
-                </div>
-                <div class="col-6">
-                    <h5 class="m-0 py-1">Data di Nascita:</h5>
-                    <p class="m-0 py-1">10/09/1992</p>
-                </div>
-                <div class="col-6">
-                    <h5 class="m-0 py-1">Età</h5>
-                    <p class="m-0 py-1">32 Anni</p>
-                </div>
-                <div class="col-6">
-                    <h5 class="m-0 py-1">Indirizzo di Residenza</h5>
-                    <p class="m-0 py-1">Via Borgo Trento Trieste 27, Camposampiero 35012 (PD), ITALIA.</p>
-                </div>
-                <div class="col-6 text-center">
-                    <div class="row m-0 justify-content-center">
-                        <div class="col-10 p-0">
-                            <h5 class=" py-1">Lingue Parlate</h5>
-                            <table class="table m-0">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Lingua</th>
-                                        <th scope="col">Livello</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>Italiano</td>
-                                        <td>Madrelingua</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Tedesco</td>
-                                        <td>Ottimo</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Inglese</td>
-                                        <td>Base</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+        <div class="row m-0 justify-content-center text-white">
+            <div class="col-6 p-0">
+                <h2 class="m-0 my-3 text-center">Contattami</h2>
 
-                <h2 class="m-0 mt-3">Contattami</h2>
-                <form class="w-50 text-start">
+                <template v-if="success">
+                    <div class="alert alert-success" role="alert">
+                        <strong>Messaggio Ricevuto</strong>
+                    </div>
+
+                </template>
+
+                <form class="text-start" @submit.prevent="sendMessage()">
+
                     <div class="mb-3">
                         <label for="name" class="form-label">Nome</label>
-                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="text-name"
-                            placeholder="Inserisci il Nome">
-                        <div id="text-name" class="form-text"></div>
+                        <input type="text" class="form-control" name="name" :class="{ 'is-invalid': errors.name }"
+                            id="name" aria-describedby="text-name" placeholder="Inserisci il Nome" v-model="name">
+                        <p class="text-danger" v-for="(error, index) in errors.name" :key="`name-error-${index}`">
+                            {{ error }}
+                        </p>
                     </div>
+
                     <div class="mb-3">
                         <label for="email" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="email"
-                            placeholder="Inserisci Email">
-                        <div id="email" class="form-text"></div>
+                        <input type="email" class="form-control" name="email" :class="{ 'is-invalid': errors.email }"
+                            id="email" aria-describedby="email" placeholder="Inserisci Email" v-model="email">
+                        <p class="text-danger" v-for="(error, index) in errors.name" :key="`name-error-${index}`">
+                            {{ error }}
+                        </p>
                     </div>
+
                     <div class="mb-3">
-                        <label for="description" class="form-label">Messaggio</label>
-                        <textarea class="form-control" id="description" placeholder="Inserisci Messaggio"></textarea>
+                        <label for="message" class="form-label">Messaggio</label>
+                        <textarea class="form-control" name="message" :class="{ 'is-invalid': errors.message }"
+                            id="message" placeholder="Inserisci Messaggio" v-model="message"></textarea>
+                        <p class="text-danger" v-for="(error, index) in errors.name" :key="`name-error-${index}`">
+                            {{ error }}
+                        </p>
                     </div>
-                    <button type="submit" class="btn btn-primary">Invia Dati</button>
+
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-primary">Invia Messaggio</button>
+                    </div>
                 </form>
             </div>
         </div>
